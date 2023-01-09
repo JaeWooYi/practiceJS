36be68a468506837e115049cfd89ddd82cb1280c
JaeWooYi <wodn0101@gmail.com>
2023-01-09 12:46:30 +0900

    Lazy

---
+++ first-project/practice/002_variable.js
@@ -0,0 +1,17 @@
+let color = "pink";
+const color2 = "red";
+var color3 = "black";
+console.log(color);
+console.log(color2);
+console.log(color3);
+
+console.log();
+console.log("------------");
+console.log();
+
+let strNum = "1";
+strNum += 1;
+console.log(strNum);
+let strNum2 = 1;
+strNum2 += 1;
+console.log(strNum2);
