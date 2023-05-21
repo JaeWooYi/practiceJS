299061f6e98c81b397ec5fdb3ac9bf50ce44aae4
JaeWooYi <wodn0101@gmail.com>
2023-05-21 11:54:28 +0900

    regret

--- study-project/0_practice/009_recentFunction.js
+++ study-project/0_practice/009_recentFunction.js
@@ -14,3 +14,30 @@ function printTest(item) {
 }
 
 test.forEach(printTest);
+
+console.log();
+console.log("------------------------------");
+console.log();
+
+// 익명함수만들기 --> 위에꺼를 이렇게도 가능 : 옛날 방법
+test.forEach(function (item) {
+  console.log(item);
+});
+
+console.log();
+console.log("------------------------------");
+console.log();
+
+// es6문법 화살표 함수로
+test.forEach((item, index) => {
+  console.log(item + " : " + index);
+});
+
+console.log();
+console.log("------------------------------");
+console.log();
+
+let data = test.map((item, index) => {
+  return item;
+});
+console.log(data);
