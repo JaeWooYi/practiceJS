b1750fb70263dc1b0bcb60e050f466e590fff2c5
JaeWooYi <wodn0101@gmail.com>
2023-06-06 13:17:53 +0900

    hungry

--- study-project/0_practice/009_recentFunction.js
+++ study-project/0_practice/009_recentFunction.js
@@ -40,4 +40,50 @@ console.log();
 let data = test.map((item, index) => {
   return item;
 });
-console.log(data);
+console.log("test1 : ", data);
+
+console.log();
+console.log("------------------------------");
+console.log();
+
+// map
+let akList = [
+  { name: "smart", age: 10, manager: true },
+  { name: "city", age: 11, manager: true },
+  { name: "association", age: 12, manager: false },
+];
+let data2 = akList.map(item => {
+  return item.name;
+});
+console.log(data2);
+
+console.log();
+console.log("------------------------------");
+console.log();
+
+// filter, some, every, find, findIndex
+// let data3 = akList.filter(item => {
+//   return item.name == "city";
+// });
+let akList2 = ["L1", "L2", "L3", "L4", "L5", "M1", "M2", "M3", "M4", "M5"];
+let data3 = akList2.filter(item => {
+  return item.startsWith("M");
+});
+
+let data4 = akList2.some(item => {
+  return item.startsWith("L");
+});
+let data5 = akList2.every(item => {
+  return item.startsWith("L");
+});
+let data6 = akList2.find(item => {
+  return item.startsWith("L3");
+});
+let data7 = akList2.findIndex(item => {
+  return item == "M1";
+});
+console.log("filter : ", data3);
+console.log("some : ", data4);
+console.log("every : ", data5);
+console.log("find : ", data6);
+console.log("find : ", data7);
